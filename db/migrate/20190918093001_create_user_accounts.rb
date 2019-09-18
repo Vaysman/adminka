358275bb149572f89b6eb6c0471fe0811bb72c86
Valit <heelmistico@gmail.com>
2019-09-18 14:27:46 +0300

    Update 20190918093001_create_user_accounts.rb

--- db/migrate/20190918093001_create_user_accounts.rb
+++ db/migrate/20190918093001_create_user_accounts.rb
@@ -1,6 +1,7 @@
 class CreateUserAccounts < ActiveRecord::Migration[6.0]
   def change
     create_table :user_accounts do |t|
+      t.email "string"
       t.integer "role", default: 0
       t.timestamps
     end
